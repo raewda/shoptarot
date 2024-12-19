5bf51eb6e3f4c3754c12e2c2cfd27835e9eeee61
wda <vorobieva2005dasha@gmail.com>
2024-12-20 01:46:01 +0700

    light/dark theme

--- src/main/resources/static/js/theme.js
+++ src/main/resources/static/js/theme.js
@@ -3,6 +3,7 @@
     const toggleTheme = () => {
         theme = !theme;
         document.getElementById("header").classList.toggle('header_dark');
+        document.getElementById("profile-text_info").classList.toggle('profile-text_info_dark');
     };
 
     document.getElementById('bubutton').addEventListener('click', toggleTheme);
